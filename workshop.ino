bc3e35c20e5bbad311bc2e767fb643902ce33b4f
Felix Engelmann <fe-github@nlogn.org>
2018-12-01 16:34:52 +0100

    App key als lsb comment

--- workshop.ino
+++ workshop.ino
@@ -75,6 +75,7 @@ void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 static const u1_t PROGMEM APPEUI[8] = ;
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
+// App Key, als lsb 
 static const u1_t PROGMEM APPKEY[16] = ;
 void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16);}
 #endif

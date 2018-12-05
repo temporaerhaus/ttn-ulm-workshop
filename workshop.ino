90160a2602101f599a6bf7f9770bfb3c5115eec8
Matthias Schneider <matthias.schneider@beached.org>
2018-12-05 20:12:00 +0100

    Update workshop.ino

--- workshop.ino
+++ workshop.ino
@@ -75,7 +75,7 @@ void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 static const u1_t PROGMEM APPEUI[8] = ;
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
-// App Key, als lsb 
+// App Key, als msb
 static const u1_t PROGMEM APPKEY[16] = ;
 void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16);}
 #endif
